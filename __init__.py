34493fd98c8bbb0dbda372874cb8dc1c58647289
alion8me <nrk0208@gmail.com>
2022-05-31 15:34:12 -0500

    add in smb checker cube projection (probably temporarily)

--- __init__.py
+++ __init__.py
@@ -15,6 +15,43 @@ bl_info = {
     "category": "UV",
 }
 
+class VIEW3D_OT_cube_project_smb_checker(bpy.types.Operator):
+    bl_idname = "uv.cube_project_smb_checker"
+    bl_label = "SMB Checker Cube Projection [smbuvt]"
+    
+    bl_options = {'REGISTER', 'UNDO'}
+    checker_size: bpy.props.IntProperty(name = "Checker Size", default=64)
+
+    def execute(self, context):
+        old_cursor_loc = bpy.context.scene.cursor.location.copy()
+        old_cursor_rot_euler = bpy.context.scene.cursor.rotation_euler.copy()
+        old_cursor_rot_quat = bpy.context.scene.cursor.rotation_quaternion.copy()
+        old_cursor_rot_mode = bpy.context.scene.cursor.rotation_mode
+        old_pivot_point = bpy.context.scene.tool_settings.transform_pivot_point
+
+        bpy.context.scene.cursor.location = (0.0, 0.0, 0.0)
+        bpy.context.scene.cursor.rotation_mode = "XYZ"
+        bpy.context.scene.cursor.rotation_euler = (0.0, 0.0, 0.0)
+        bpy.context.scene.tool_settings.transform_pivot_point = "CURSOR"
+        
+        #find scale factor
+        scale_factor = bpy.context.active_object.active_material.node_tree.nodes.get("Image Texture").image.size[0]
+        
+        bpy.ops.uv.cube_project(
+            cube_size=1.0*(self.checker_size*4.0)/scale_factor,
+            correct_aspect=True,
+            clip_to_bounds=False,
+            scale_to_bounds=False
+        )
+
+        bpy.context.scene.cursor.location = old_cursor_loc
+        bpy.context.scene.cursor.rotation_mode = old_cursor_rot_mode
+        bpy.context.scene.cursor.rotation_euler = old_cursor_rot_euler
+        bpy.context.scene.cursor.rotation_quaternion = old_cursor_rot_quat
+        bpy.context.scene.tool_settings.transform_pivot_point = old_pivot_point
+
+        return {"FINISHED"}
+
         
 class VIEW3D_OT_checker_FAQ(bpy.types.Operator):
     bl_idname = "uv.checker_faq"
@@ -210,8 +247,9 @@ class VIEW3D_OT_smart_FAQ(bpy.types.Operator):
 def menu_func(self, context):
     self.layout.operator(VIEW3D_OT_checker_FAQ.bl_idname)
     self.layout.operator(VIEW3D_OT_smart_FAQ.bl_idname)
+    self.layout.operator(VIEW3D_OT_cube_project_smb_checker)
 
-CLASSES = [VIEW3D_OT_checker_FAQ, VIEW3D_OT_smart_FAQ]
+CLASSES = [VIEW3D_OT_checker_FAQ, VIEW3D_OT_smart_FAQ, VIEW3D_OT_cube_project_smb_checker]
 
 # Register
 def register():
